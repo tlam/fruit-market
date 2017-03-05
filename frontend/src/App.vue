a3750f57b3e47945355bb9dca56ece738e614b5b
Thierry <lamthierry@gmail.com>
2017-03-04 23:47:12 -0500

    Using socketio

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
+    <start-game></start-game>
     <hello></hello>
   </div>
 </template>
 
 <script>
+
 import Hello from './components/Hello.vue'
+import StartGame from './components/StartGame.vue'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Hello,
+    StartGame
   }
 }
 </script>
---
+++ frontend/src/components/StartGame.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b>Fruit Market</b>
+    <select id="players">
+      <option val="2">2</option>
+      <option val="3">3</option>
+    </select>
+    <button v-on:click="start">Start</button>
+    <button v-on:click="resume">Resume</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'start-game',
+  data () {
+    return {
+      msg: 'Start game'
+    }
+  },
+  methods: {
+    start: function(event) {
+      this.$socket.emit('start game', {
+        numPlayers: 2
+      });
+    },
+    resume: function(event) {
+      this.$socket.emit('resume', {});
+    }
+  }
+}
+</script>
