5dada19f322c0b0ffe7564cb7a291d1a4785f8be
Thierry <lamthierry@gmail.com>
2017-03-05 15:25:50 -0500

    Listening to events from the server

--- frontend/src/components/StartGame.vue
+++ frontend/src/components/StartGame.vue
@@ -7,6 +7,10 @@
     </select>
     <button v-on:click="start">Start</button>
     <button v-on:click="resume">Resume</button>
+
+    <div class="alert alert-info" role="alert">
+      <div>{{msg}}</div>
+    </div>
   </div>
 </template>
 
@@ -15,7 +19,40 @@ export default {
   name: 'start-game',
   data () {
     return {
-      msg: 'Start game'
+      board: {},
+      currentPlayer: {},
+      fruits:['dragonFruit', 'kiwi', 'olive', 'strawberry', 'plum'],
+      msg: 'Start game',
+      paid: {
+        dragonFruit: 0,
+        kiwi: 0,
+        olive: 0,
+        strawberry: 0,
+        plum: 0
+      },
+      playerDevelopment: {
+        dragonFruit: 0,
+        kiwi: 0,
+        olive: 0,
+        strawberry: 0,
+        plum: 0
+      },
+      selectedCard: null,
+      taken: {
+        dragonFruit: 0,
+        kiwi: 0,
+        olive: 0,
+        strawberry: 0,
+        plum: 0
+      },
+      tokenSpent: {
+        dragonFruit: 0,
+        kiwi: 0,
+        olive: 0,
+        strawberry: 0,
+        plum: 0,
+        pineapple: 0
+      }
     }
   },
   methods: {
@@ -24,8 +61,41 @@ export default {
         numPlayers: 2
       });
     },
+    reset: function() {
+      for (var fruit in this.paid) {
+        this.paid[fruit] = 0;
+      }
+      for (var fruit in this.taken) {
+        this.taken[fruit] = 0;
+      }
+      for (var fruit in this.tokenSpent) {
+        this.tokenSpent[fruit] = 0;
+      }
+      this.selectedCard = null;
+    },
     resume: function(event) {
       this.$socket.emit('resume', {});
+    }
+  },
+  sockets: {
+    'display cards': function(data) {
+      console.log('display cards');
+      this.reset();
+      this.board = data.board;
+      this.msg = data.msg;
+      for (let player of this.board.players) {
+        if (player.isTurn) {
+          this.currentPlayer = player;
+          this.playerDevelopment.dragonFruit = player.dragonFruitDevelopment;
+          this.playerDevelopment.kiwi = player.kiwiDevelopment;
+          this.playerDevelopment.olive = player.oliveDevelopment;
+          this.playerDevelopment.strawberry = player.strawberryDevelopment;
+          this.playerDevelopment.plum = player.plumDevelopment;
+        }
+      }
+    },
+    'status': function(data) {
+      this.msg = data.msg;
     }
   }
 }
